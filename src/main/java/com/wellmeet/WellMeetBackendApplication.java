e997d15379b7e7b2ae120adadf95a9a24aa8d6ce
unifolio0 <boho82ha@gmail.com>
2025-06-24 11:42:23 +0900

    config: 백엔드 프로젝트 환경 설정

---
+++ src/main/java/com/wellmeet/WellMeetBackendApplication.java
@@ -0,0 +1,12 @@
+package com.wellmeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WellMeetBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WellMeetBackendApplication.class, args);
+	}
+}
---
+++ src/test/java/com/wellmeet/WellMeetBackendApplicationTests.java
@@ -0,0 +1,12 @@
+package com.wellmeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WellMeetBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
