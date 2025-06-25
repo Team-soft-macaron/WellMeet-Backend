453ee96e5e0b5e7a4647584db6dbad5f06503bad
unifolio0 <boho82ha@gmail.com>
2025-06-25 15:37:06 +0900

    feat: CrawlingReview 엔티티

---
+++ src/main/java/com/wellmeet/restaurant/domain/crawlingreview/domain/CrawlingReview.java
@@ -0,0 +1,26 @@
+package com.wellmeet.restaurant.domain.crawlingreview.domain;
+
+import com.wellmeet.restaurant.domain.Restaurant;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CrawlingReview {
+
+    @Id
+    private String id;
+    private String content;
+    private double rating;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "restaurant_id")
+    private Restaurant restaurant;
+}
