bad05039e3af4d5a83cd818d9fb242787d3292c2
unifolio0 <boho82ha@gmail.com>
2025-06-25 15:33:49 +0900

    refactor: 생성자 어노테이션 수정

--- src/main/java/com/wellmeet/member/domain/Member.java
+++ src/main/java/com/wellmeet/member/domain/Member.java
@@ -5,15 +5,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.RequiredArgsConstructor;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
 @Entity
-@RequiredArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String nickname;
 }
