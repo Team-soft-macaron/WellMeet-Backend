20e40d517e2f3494ef236944b3f08a4905ce09d3
unifolio0 <boho82ha@gmail.com>
2025-06-25 15:42:16 +0900

    feat: Review 엔티티

---
+++ src/main/java/com/wellmeet/restaurant/domain/review/domain/Review.java
@@ -0,0 +1,34 @@
+package com.wellmeet.restaurant.domain.review.domain;
+
+import com.wellmeet.member.domain.Member;
+import com.wellmeet.restaurant.domain.Restaurant;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String content;
+    private double rating;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "restaurant_id")
+    private Restaurant restaurant;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+}
