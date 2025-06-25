ff97cc009ec2b03faa9f4e9c9fb8c21a2ea08e29
unifolio0 <boho82ha@gmail.com>
2025-06-25 15:40:20 +0900

    feat: CrawlingReviewAtmosphere 엔티티

---
+++ src/main/java/com/wellmeet/restaurant/domain/crawlingreview/domain/CrawlingReviewAtmosphere.java
@@ -0,0 +1,30 @@
+package com.wellmeet.restaurant.domain.crawlingreview.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CrawlingReviewAtmosphere {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "crawling_review_id")
+    private CrawlingReview crawlingReview;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "atmosphere_id")
+    private Atmosphere atmosphere;
+}
