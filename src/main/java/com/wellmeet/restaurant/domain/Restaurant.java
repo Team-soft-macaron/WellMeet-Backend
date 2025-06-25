4b21d0ea52e3e77fcd59c59f2698bfbbecaca9f0
unifolio0 <boho82ha@gmail.com>
2025-06-25 15:34:07 +0900

    feat: Restaurant 엔티티

---
+++ src/main/java/com/wellmeet/restaurant/domain/Restaurant.java
@@ -0,0 +1,21 @@
+package com.wellmeet.restaurant.domain;
+
+import com.wellmeet.common.domain.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Restaurant extends BaseEntity {
+
+    @Id
+    private Long id;
+    private String name;
+    private String address;
+    private double latitude;
+    private double longitude;
+}
