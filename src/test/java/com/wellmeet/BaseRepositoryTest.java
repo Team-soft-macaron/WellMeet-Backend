7047197cc70235712fcc35d32be7ab03ac4d4490
SANGHUN OH <121424793+unifolio0@users.noreply.github.com>
2025-06-30 20:53:36 +0900

    SCRUM-23 CI를 구축한다 (#6)
    * feat: CI 구축
    
    * config: jacoco 추가
    
    * chore: ci 트리거 수정

--- src/test/java/com/wellmeet/BaseRepositoryTest.java
+++ src/test/java/com/wellmeet/BaseRepositoryTest.java
@@ -12,14 +12,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ActiveProfiles;
 
 @Import({
         JpaAuditingConfig.class,
         CrawlingReviewGenerator.class
 })
 @DataJpaTest
-@ActiveProfiles("test")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public abstract class BaseRepositoryTest {
 
