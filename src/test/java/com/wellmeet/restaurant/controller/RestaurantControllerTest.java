7a5b9dbadf55b165d63ffbdf4870e3fff8886842
ezcolin2 <growth_s@naver.com>
2025-07-01 14:40:33 +0900

    fix: findRestaurantsOrderedByVibeRatio 테스트 코드 Restaurant 생성자에 mainImage 추가

--- src/test/java/com/wellmeet/restaurant/controller/RestaurantControllerTest.java
+++ src/test/java/com/wellmeet/restaurant/controller/RestaurantControllerTest.java
@@ -6,43 +6,47 @@ import static com.wellmeet.restaurant.domain.crawlingreview.domain.VibeName.LIVE
 import static com.wellmeet.restaurant.domain.crawlingreview.domain.VibeName.MODERN;
 import static com.wellmeet.restaurant.domain.crawlingreview.domain.VibeName.values;
 import static org.assertj.core.api.Assertions.assertThat;
-
 import com.wellmeet.BaseControllerTest;
 import com.wellmeet.restaurant.domain.Restaurant;
 import com.wellmeet.restaurant.domain.crawlingreview.domain.Vibe;
 import com.wellmeet.restaurant.domain.crawlingreview.domain.VibeName;
 import com.wellmeet.restaurant.dto.RecommendRestaurantRequest;
 import com.wellmeet.restaurant.dto.RecommendRestaurantResponse;
-import io.restassured.http.ContentType;
 import java.util.Arrays;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 
 class RestaurantControllerTest extends BaseControllerTest {
-
     private static final double LATITUDE = 132.1;
     private static final double LONGITUDE = 123.1;
+    private static final String MAIN_IMAGE = "https://example.com/restaurant.jpg";
 
-    @Test
-    void getRecommendRestaurants() {
+    @BeforeEach
+    void setEnvironment() {
         Arrays.stream(values())
                 .forEach(vibeName -> vibeRepository.save(new Vibe(vibeName.name())));
+    }
 
-        Restaurant restaurant1 = new Restaurant("restaurant1", "address1", LATITUDE, LONGITUDE);
+    @Test
+    void findRestaurantsOrderedByVibeRatio() {
+        Restaurant restaurant1 = new Restaurant("restaurant1", "address1", LATITUDE, LONGITUDE, MAIN_IMAGE);
         Restaurant savedRestaurant1 = restaurantRepository.save(restaurant1);
         createCrawlingReviews(savedRestaurant1, CLASSIC, CLASSIC, CLASSIC, CLEAN, LIVELY);
 
-        Restaurant restaurant2 = new Restaurant("restaurant2", "address2", LATITUDE, LONGITUDE);
+        Restaurant restaurant2 = new Restaurant("restaurant2", "address2", LATITUDE, LONGITUDE, MAIN_IMAGE);
         Restaurant savedRestaurant2 = restaurantRepository.save(restaurant2);
         createCrawlingReviews(savedRestaurant2, CLASSIC, CLASSIC, LIVELY, MODERN);
 
-        Restaurant restaurant3 = new Restaurant("restaurant3", "address3", LATITUDE, LONGITUDE);
+        Restaurant restaurant3 = new Restaurant("restaurant3", "address3", LATITUDE, LONGITUDE, MAIN_IMAGE);
         Restaurant savedRestaurant3 = restaurantRepository.save(restaurant3);
         createCrawlingReviews(savedRestaurant3, CLASSIC, CLASSIC, LIVELY);
 
-        Restaurant restaurant4 = new Restaurant("restaurant4", "address4", LATITUDE, LONGITUDE);
+        Restaurant restaurant4 = new Restaurant("restaurant4", "address4", LATITUDE, LONGITUDE, MAIN_IMAGE);
         Restaurant savedRestaurant4 = restaurantRepository.save(restaurant4);
         createCrawlingReviews(savedRestaurant4, LIVELY, LIVELY, LIVELY);
+
 
         RecommendRestaurantRequest request = new RecommendRestaurantRequest(CLASSIC);
 
@@ -52,7 +56,6 @@ class RestaurantControllerTest extends BaseControllerTest {
                 .when().get("/api/restaurant/recommend")
                 .then().statusCode(HttpStatus.OK.value())
                 .extract().as(RecommendRestaurantResponse[].class);
-
         assertThat(responses).hasSize(3);
         assertThat(responses[0].getId()).isEqualTo(savedRestaurant3.getId());
         assertThat(responses[1].getId()).isEqualTo(savedRestaurant1.getId());
