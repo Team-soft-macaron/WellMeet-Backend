8b37a64d3220fa41d52beb5d43f7b7a0e24a9fa5
unifolio0 <boho82ha@gmail.com>
2025-07-31 15:57:56 +0900

    refactor: 패키지 이동

--- api-owner/src/main/java/com/wellmeet/owner/OwnerService.java
+++ api-owner/src/main/java/com/wellmeet/owner/OwnerService.java
@@ -1,4 +1,4 @@
-package com.wellmeet.ownermanage;
+package com.wellmeet.owner;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
--- api-owner/src/main/java/com/wellmeet/ownermanage/owner/OwnerService.java
+++
@@ -1,9 +0,0 @@
-package com.wellmeet.ownermanage.owner;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class OwnerService {
-}
