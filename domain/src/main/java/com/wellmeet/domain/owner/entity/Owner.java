7e0cb7fd3ffa74780e9e942d0f0528520f3f1e77
unifolio0 <boho82ha@gmail.com>
2025-08-03 16:46:18 +0900

    refactor: 필드로 식당 제외

--- domain/src/main/java/com/wellmeet/domain/owner/entity/Owner.java
+++ domain/src/main/java/com/wellmeet/domain/owner/entity/Owner.java
@@ -1,14 +1,10 @@
 package com.wellmeet.domain.owner.entity;
 
 import com.wellmeet.domain.common.BaseEntity;
-import com.wellmeet.domain.restaurant.entity.Restaurant;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -32,15 +28,10 @@ public class Owner extends BaseEntity {
     private boolean reservationEnabled;
     private boolean reviewEnabled;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "restaurant_id")
-    private Restaurant restaurant;
-
-    public Owner(String name, String email, Restaurant restaurant) {
+    public Owner(String name, String email) {
         this.name = name;
         this.email = email;
         this.reservationEnabled = true;
         this.reviewEnabled = true;
-        this.restaurant = restaurant;
     }
 }
