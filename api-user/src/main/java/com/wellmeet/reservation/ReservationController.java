328cd3ed5e6dc7356c3ec4be8398ab01db71c532
unifolio0 <boho82ha@gmail.com>
2025-10-31 21:13:39 +0900

    refactor: 임시로 빈 이름 변경

--- api-user/src/main/java/com/wellmeet/reservation/ReservationController.java
+++ api-user/src/main/java/com/wellmeet/reservation/ReservationController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/user/reservation")
-@RestController
+@RestController("domainRestaurantController")
 @RequiredArgsConstructor
 public class ReservationController {
 
