e3dc8ddeef344864e175c1da5eba43425f329bbc
unifolio0 <boho82ha@gmail.com>
2025-11-06 12:23:53 +0900

    chore: sql 수정

--- domain-member/src/main/resources/db/migration/V1__init_schema.sql
+++ domain-member/src/main/resources/db/migration/V1__init_schema.sql
@@ -1,15 +1,19 @@
 -- Member 테이블
 CREATE TABLE member
 (
-    id           VARCHAR(255) PRIMARY KEY,
-    name         VARCHAR(255) NOT NULL,
-    nickname     VARCHAR(255) NOT NULL,
-    email        VARCHAR(255) NOT NULL,
-    phone_number VARCHAR(255) NOT NULL,
-    created_at   DATETIME(6) NOT NULL,
-    updated_at   DATETIME(6) NOT NULL,
+    id                  VARCHAR(255) PRIMARY KEY,
+    name                VARCHAR(255) NOT NULL,
+    nickname            VARCHAR(255) NOT NULL,
+    email               VARCHAR(255) NOT NULL,
+    phone               VARCHAR(255) NOT NULL,
+    reservation_enabled BOOLEAN      NOT NULL DEFAULT TRUE,
+    remind_enabled      BOOLEAN      NOT NULL DEFAULT TRUE,
+    review_enabled      BOOLEAN      NOT NULL DEFAULT TRUE,
+    is_vip              BOOLEAN      NOT NULL DEFAULT FALSE,
+    created_at          DATETIME(6) NOT NULL,
+    updated_at          DATETIME(6) NOT NULL,
     UNIQUE KEY unique_email (email),
-    UNIQUE KEY unique_phone_number (phone_number)
+    UNIQUE KEY unique_phone (phone)
 );
 
 -- FavoriteRestaurant 테이블
